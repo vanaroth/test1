05ca1b898e2d9a0f9df1d05f1bf9d16ff772a80d
play.js <git@gituser.com>
2020-10-28 14:48:30 +0100

    second

--- src/app.jsx
+++ src/app.jsx
@@ -8,10 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/app.jsx</code> and tap.... on the play button.
-        </p>
+        Test
         <a
           className="App-link"
           href="https://reactjs.org"
